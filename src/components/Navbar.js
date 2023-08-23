0ec6606911584d69bb43367af1bc1a10084ea30e
cliff <cliffmudigo5@gmail.com>
2023-08-23 15:32:08 +0300

    add styles and logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import { Search } from '@mui/icons-material';
+import { Badge } from '@mui/icons-material';
+import { Search, ShoppingCartOutlined} from '@mui/icons-material';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -34,16 +35,27 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   /* Your input styles here */
+  border: none;
 `;
 
 const Center = styled.div`
   flex: 1;
+  text-align:center;
 `;
 
 const Right = styled.div`
   flex: 1;
+  display:flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
-
+const Logo = styled.h1`
+font-weight:bold;
+`;
+const MenuItem =styled.div`
+font-size:14px;
+cursor:pointer;
+`
 const Navbar = () => {
   return (
     <Container>
@@ -55,8 +67,16 @@ const Navbar = () => {
             <Search />
           </SearchContainer>
         </Left>
-        <Center>center</Center>
-        <Right>right</Right>
+        <Center><Logo>BALEZ.</Logo></Center>
+        <Right>
+         <MenuItem>REGISTER</MenuItem>
+         <MenuItem>SIGN IN</MenuItem>
+         <MenuItem>
+         <Badge badgeContent={4} color="primary">
+             <ShoppingCartOutlined />
+         </Badge>
+         </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );

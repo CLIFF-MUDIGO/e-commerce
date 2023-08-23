35f8e8cd5f863c590dd232859a1f36e25e956e66
cliff <cliffmudigo5@gmail.com>
2023-08-23 16:09:32 +0300

    slider

---
+++ src/components/Slider.js
@@ -0,0 +1,30 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material"
+import styled from "styled-components"
+
+const Container = styled.div`
+width:100%;
+height:100vh;
+display: flex;
+
+`
+const Arrow = styled.div`
+width:50px;
+height:50px;
+background-color:#fff7f7;
+border-radius:50%;
+`
+
+const Slider = () => {
+  return (
+   <Container>
+    <Arrow>
+    <ArrowLeftOutlined />
+    </Arrow>
+    <Arrow>
+    <ArrowRightOutlined />
+    </Arrow>
+   </Container>
+  )
+}
+
+export default Slider

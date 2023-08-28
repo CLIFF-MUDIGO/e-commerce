46bf695807438f51fdab357f6108352ab96798f6
cliff <cliffmudigo5@gmail.com>
2023-08-28 11:38:39 +0300

    product and products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,54 @@
+import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 import styled  from "styled-components"
 
 
 const Container = styled.div`
+flex:1;
+margin:5px;
+min-width: 280px;
+height: 350px;
+display:flex;
+align-items:center;
+justify-content:center;
+background-color:#f5fbfd;
+border-radius:10px;
+position:relative;
+
+`;
+const Circle = styled.div`
+width:200px;
+height:200px;
+border-radius:50%;
+background-color: white;
+position : absolute;
+
+
+
+`;
+const Image = styled.img`
+height: 75%;
+z-index:2;
+
+`;
+const Info = styled.div`
+width:100%;
+height:100%;
+position:absolute;
+top:0;
+left:0;
+display:flex;
+align-items:center;
+justify-content:center;
+background-color: rgba(0,0,0,0.2);
+z-index:3;
+`;
+const Icon = styled.div`
+width:40px;
+height:40px;
+border-radius:50%;
+background-color:white;
+display:flex;
+align-items:center;
 
 `;
 
@@ -12,8 +59,15 @@ const Product = ({item}) => {
        <Image src={item.img}/>
        <Info>
         <Icon>
-            
+            <ShoppingCartOutlined />
         </Icon>
+        <Icon>
+            < SearchOutlined />
+        </Icon>
+        <Icon>
+            < FavoriteBorderOutlined />
+        </Icon>
+
        </Info>
     </Container>
   )

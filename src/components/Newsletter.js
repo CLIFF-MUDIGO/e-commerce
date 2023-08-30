b9f3f19b3b1c699a8699845e1432daf3d5a7c1e0
cliff <cliffmudigo5@gmail.com>
2023-08-30 09:56:10 +0300

    styles

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -7,18 +7,35 @@ margin:5px;
 min-width: 280px;
 height: 350px;
 display:flex;
+flex-direction: column;
 align-items:center;
 justify-content:center;
-background-color:#f5fbfb;
+background-color:#fcf5f5;
 border-radius:10px;
 position:relative;
 cursor:pointer;
 transition: all 0.5s ease;
 
 `;
-const Title = styled.h1``;
-const Desc = styled.div``;
-const InputContainer = styled.div``;
+const Title = styled.h1`
+font-size: 70px;
+margin-buttom:20px;
+`;
+const Desc = styled.div`
+font-size:24px;
+font-weight:300;
+margin-buttom:20px;
+`;
+const InputContainer = styled.div`
+width: 50%;
+height: 40%;
+background-color:white;
+dispay:flex;
+justify-content:space-between;
+align-items:center;
+border:1px solid lightgray;
+
+`;
 const Input = styled.input``;
 const Button = styled.button``;
 

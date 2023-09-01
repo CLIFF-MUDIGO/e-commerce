76f123e49ecceff71eecb4e7e59c4af2fa46cd71
cliff <cliffmudigo5@gmail.com>
2023-09-01 11:57:06 +0300

    single products

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { Instagram, Pinterest, Twitter } from "@mui/icons-material";
+import { Call, Email, Instagram, LocationOn, Pinterest, Twitter } from "@mui/icons-material";
 import { styled } from "styled-components"
 
 const Container = styled.div`
@@ -6,7 +6,7 @@ display:flex;
 
 
 `;
-const Left= styled.div`
+const Left = styled.div`
 flex:1;
 display:flex;
 flex-direction:column;
@@ -26,49 +26,105 @@ width:40px;
 height:40px;
 border-radius:50%;
 color:white;
-background-color:#${props=>props.color}
+background-color:#${props => props.color};
+display:flex;
+align-items:center;
+justify-content:center;
+margin-right:20px;
+
 
 `;
 
 
 const Center = styled.div`
-
 flex:1;
+padding:20px;
 `;
 const Right = styled.div`
 flex:1;
+padding:20px;
+`;
+const Title = styled.h3`
+margin-bottom:30px;
 
 `;
+
+const List = styled.ul`
+margin:0;
+padding:0;
+list-style:none;
+display-flex;
+flex-wrap:wrap;
+
+`;
+
+const ListItem = styled.li`
+width:50%;
+margin-bottom:10px;
+
+`;
+
+const ContactItem = styled.div`
+margin-bottom: 20px;
+display:flex;
+align-items:center;
+
+
+`;
+const Payments = styled.img`
+width:50%;
+`;
 const Footer = () => {
-  return (
-   <Container>
-        <Left>
-         <Logo>BALEZ.</Logo>
-         <Desc>
-            There are many variations of passages of lorem ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which dont look even slightly believed
-         </Desc>
-         <SocialContainer>
-            <SocialIcon color="E4405F"> 
-                <Instagram />
-            </SocialIcon>
-            <SocialIcon color="55ACEE">
-                <Twitter />
-            </SocialIcon>
-            <SocialIcon color="E60023">
-                <Pinterest />
-            </SocialIcon>
-         </SocialContainer>
-        </Left>
-        <Center>
-
-        </Center>
-        <Right>
-
-        </Right> 
-   </Container>
-  )
+    return (
+        <Container>
+            <Left>
+                <Logo>BALEZ.</Logo>
+                <Desc>
+                    There are many variations of passages of lorem ipsum available, but
+                    the majority have suffered alteration in some form, by injected
+                    humour, or randomised words which dont look even slightly believed
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="E4405F">
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color="E60023">
+                        <Pinterest />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Cart</ListItem>
+                <ListItem>Man Fashion</ListItem>
+                <ListItem>Woman Fashion</ListItem>
+                <ListItem>Accesories</ListItem>
+                <ListItem>My Accoint</ListItem>
+                <ListItem>Order Tracking</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+              <Title>Contact</Title>
+              <ContactItem><LocationOn style={{marginRight:"10px"}}/>
+              Nairobi, Kenya
+              </ContactItem>
+              <ContactItem><Call style={{marginRight:"10px"}}/>
+              +254 705949828
+              </ContactItem>
+              <ContactItem><Email style={{marginRight:"10px"}}/>
+              cmudigo5@gmail.com
+              </ContactItem>
+              <Payments src="//i.ibb.co/Qfvn4z6/payment.png"/>
+            </Right>
+        </Container>
+    )
 };
 
 export default Footer
---
+++ src/pages/Product.js
@@ -0,0 +1,63 @@
+import { styled } from "styled-components";
+import Annoucement from "../components/Annoucement";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+padding:50px;
+display:flex;
+`;
+const ImgContainer = styled.div`
+flex:1;
+`;
+const Image = styled.img`
+width:100%;
+height:90vh;
+object-fit:cover;
+`;
+const InfoContainer = styled.div`
+flex:1;
+padding:0px 50px;
+`;
+const Title = styled.h1`
+font-weigh:200;`;
+const Desc = styled.p`
+margin:20px 0px;`;
+const Price = styled.span`
+font-weight:100;
+font-size:40px;
+`;
+
+const Product = () => {
+  return (
+    <Container>
+     <Navbar/>
+     <Annoucement/>
+     <Wrapper>
+        <ImgContainer>
+        <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+            <Title>
+               Denim Jumpsuit
+            </Title>
+            <Desc>
+            Versatile denim jumpsuit for a stylish and comfortable look. Features include durable denim fabric, 
+            easy-to-wear design with adjustable straps and pockets
+            </Desc>
+            <Price>
+             $ 20
+            </Price>
+            
+        </InfoContainer>
+     </Wrapper>
+     <Newsletter/>
+     <Footer/>
+    </Container>
+  )
+}
+
+export default Product

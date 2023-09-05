d6f68a152372174846486ed352b75b50b504a2ff
cliff <cliffmudigo5@gmail.com>
2023-09-05 17:01:30 +0300

    register and login forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from './pages/ProductList';
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div>
-      <Product/>
+      <Login/>
     </div>
   )
 }
